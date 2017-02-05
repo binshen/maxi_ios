36dc3c15f3c3bd277f10f29f1996e824e48b845f
bin.shen <bluelazysb@hotmail.com>
2017-02-05 20:52:48 +0800

    updates

--- maxi_ios/Controller/Login/NaviPage.m
+++ maxi_ios/Controller/Login/NaviPage.m
@@ -4,6 +4,8 @@
 //
 
 #import "NaviPage.h"
+#import "LoginPage.h"
+#import "RegisterPage.h"
 
 @interface NaviPage() {
 
@@ -26,28 +28,44 @@
     //self.navigationController.navigationBarHidden = NO;
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
 
     UIImage *image = [UIImage imageNamed:@"bj"];
     self.view.layer.contents = (id) image.CGImage;    // 如果需要背景透明加上下面这句
     self.view.layer.backgroundColor = [UIColor clearColor].CGColor;
 
-    btnLoginView = [[UIImageView alloc] initWithFrame:CGRectMake(10, SCREEN_HEIGHT-75, 140, 40)];
+    btnRegisterView = [[UIImageView alloc] initWithFrame:CGRectMake(10, SCREEN_HEIGHT-75, 140, 40)];
+    btnRegisterView.image = [UIImage imageNamed:@"btn_register.png"];
+    btnRegisterView.userInteractionEnabled = YES;
+    [btnRegisterView.layer setMasksToBounds:YES];
+    [btnRegisterView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(goToRegisterPage:)]];
+    [self.view addSubview:btnRegisterView];
+
+    btnLoginView = [[UIImageView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH-150, SCREEN_HEIGHT-75, 140, 40)];
     btnLoginView.image = [UIImage imageNamed:@"btn_login.png"];
     btnLoginView.userInteractionEnabled = YES;
     [btnLoginView.layer setMasksToBounds:YES];
+    [btnRegisterView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(goToLoginPage:)]];
     [self.view addSubview:btnLoginView];
-
-    btnRegisterView = [[UIImageView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH-150, SCREEN_HEIGHT-75, 140, 40)];
-    btnRegisterView.image = [UIImage imageNamed:@"btn_login.png"];
-    btnRegisterView.userInteractionEnabled = YES;
-    [btnRegisterView.layer setMasksToBounds:YES];
-    [self.view addSubview:btnRegisterView];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)goToRegisterPage:(UIGestureRecognizer *)gestureRecognizer
+{
+    RegisterPage * registPage = [[RegisterPage alloc] initIsFirstPage:NO];
+    [self.navigationController pushViewController:registPage animated:YES];
+}
+
+-(void)goToLoginPage:(UIGestureRecognizer *)gestureRecognizer
+{
+    LoginPage * loginPage = [[LoginPage alloc] initIsFirstPage:NO];
+    [self.navigationController pushViewController:loginPage animated:YES];
 }
 @end
