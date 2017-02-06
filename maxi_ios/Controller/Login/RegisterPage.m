db4a8873f8c07053588a9a656ced56474239009c
bin.shen <bluelazysb@hotmail.com>
2017-02-06 23:28:57 +0800

    updates

--- maxi_ios/Controller/Login/RegisterPage.m
+++ maxi_ios/Controller/Login/RegisterPage.m
@@ -30,30 +30,49 @@
     [self setNavigationLeft:@"返回" sel:@selector(backAction)];
 
     userPhoneTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 25, SCREEN_WIDTH-40, 35)];
-    userPhoneTextField.placeholder = @"请输入手机号";
-    UIImageView *userPhoneImageView = [[UIImageView alloc] initWithFrame:CGRectMake(-20, 0, 32, 30)];
-    userPhoneImageView.image = [UIImage imageNamed:@"shouji"];
-    userPhoneTextField.leftView = userPhoneImageView;
+    userPhoneTextField.placeholder = @"  请输入手机号";
+    UIImageView *textImageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(-20, 0, 32, 30)];
+    textImageView1.image = [UIImage imageNamed:@"shouji"];
+    userPhoneTextField.leftView = textImageView1;
     userPhoneTextField.leftViewMode = UITextFieldViewModeAlways;
-    //userPhoneTextField.borderStyle = UITextBorderStyleRoundedRect;
     userPhoneTextField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     userPhoneTextField.delegate = self;
     [userPhoneTextField setSecureTextEntry:YES];
     [self.view addSubview:userPhoneTextField];
 
-    UIView *lineView = [[UIView alloc]initWithFrame:CGRectMake(0, 36, SCREEN_WIDTH-40, 1)];
-    lineView.backgroundColor = [UIColor blueColor];
-    [userPhoneTextField addSubview:lineView];
+    UIView *lineView1 = [[UIView alloc]initWithFrame:CGRectMake(0, 36, SCREEN_WIDTH-40, 1)];
+    lineView1.backgroundColor = kUIColorFromRGB(0x1b96fe);
+    [userPhoneTextField addSubview:lineView1];
 
-    userCodeTextField = [[WPTextField alloc] initWithFrame:CGRectMake(20, 75, SCREEN_WIDTH-150, 35)];
-    userCodeTextField.placeholder = @"请输入验证码";
+    userCodeTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 75, SCREEN_WIDTH-150, 35)];
+    userCodeTextField.placeholder = @"  请输入验证码";
+    UIImageView *textImageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(-20, 0, 32, 30)];
+    textImageView2.image = [UIImage imageNamed:@"code"];
+    userCodeTextField.leftView = textImageView2;
+    userCodeTextField.leftViewMode = UITextFieldViewModeAlways;
+    userCodeTextField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     userCodeTextField.delegate = self;
+    [userCodeTextField setSecureTextEntry:YES];
     [self.view addSubview:userCodeTextField];
 
-    userPasswordTextField = [[WPTextField alloc] initWithFrame:CGRectMake(20, 125, SCREEN_WIDTH-40, 35)];
-    userPasswordTextField.placeholder = @"请输入登录密码";
+    UIView *lineView2 = [[UIView alloc]initWithFrame:CGRectMake(0, 36, SCREEN_WIDTH-150, 1)];
+    lineView2.backgroundColor = kUIColorFromRGB(0x1b96fe);
+    [userCodeTextField addSubview:lineView2];
+
+    userPasswordTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 125, SCREEN_WIDTH-40, 35)];
+    userPasswordTextField.placeholder = @"  请输入6-16位密码";
+    UIImageView *textImageView3 = [[UIImageView alloc] initWithFrame:CGRectMake(-20, 0, 32, 30)];
+    textImageView3.image = [UIImage imageNamed:@"mima"];
+    userPasswordTextField.leftView = textImageView3;
+    userPasswordTextField.leftViewMode = UITextFieldViewModeAlways;
+    userPasswordTextField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     userPasswordTextField.delegate = self;
+    [userPasswordTextField setSecureTextEntry:YES];
     [self.view addSubview:userPasswordTextField];
+
+    UIView *lineView3 = [[UIView alloc]initWithFrame:CGRectMake(0, 36, SCREEN_WIDTH-40, 1)];
+    lineView3.backgroundColor = kUIColorFromRGB(0x1b96fe);
+    [userPasswordTextField addSubview:lineView3];
 
     requestCodeBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     requestCodeBtn.frame = CGRectMake(SCREEN_WIDTH-120, 50, 120, 40);
