9d6ac6402520bd5e86fa7e322eabb5fec6de0b1c
bin.shen <bluelazysb@hotmail.com>
2017-02-13 22:36:58 +0800

    updates

--- maxi_ios/Controller/Device/DeviceMainPage.m
+++ maxi_ios/Controller/Device/DeviceMainPage.m
@@ -18,6 +18,7 @@
     self.title = @"淼溪净水";
     NSDictionary *dic = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:[UIColor blackColor], [UIFont systemFontOfSize:20 weight:20], nil] forKeys:[NSArray arrayWithObjects:NSForegroundColorAttributeName, NSFontAttributeName, nil]];
     self.navigationController.navigationBar.titleTextAttributes = dic;
+
     UIImage* backImage = [UIImage imageNamed:@"btn_back"];
     UIButton* backButton= [[UIButton alloc] initWithFrame:CGRectMake(0,0,16,20)];
     [backButton setBackgroundImage:backImage forState:UIControlStateNormal];
@@ -25,9 +26,16 @@
     UIBarButtonItem* leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     self.navigationItem.leftBarButtonItem = leftBarButtonItem;
 
-
+    UIImage* addImage = [UIImage imageNamed:@"btn_add"];
+    UIButton* addButton= [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH-35,0,20,20)];
+    [addButton setBackgroundImage:addImage forState:UIControlStateNormal];
+    [addButton addTarget:self action:@selector(addAction:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem* rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:addButton];
+    self.navigationItem.rightBarButtonItem = rightBarButtonItem;
 
 }
 
+-(void)addAction:(id)sender {
+}
 
 @end
