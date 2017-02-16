e2522fa82b2ac0e4fec6911e66d6efef1f5a53e7
沈斌 <bluelazysb@hotmail.com>
2017-02-16 22:48:37 +0800

    updates

--- maxi_ios/Controller/Device/DeviceDetailPage.m
+++ maxi_ios/Controller/Device/DeviceDetailPage.m
@@ -34,7 +34,39 @@
     UIBarButtonItem* rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:addButton];
     self.navigationItem.rightBarButtonItem = rightBarButtonItem;
 
+    UIImage *bg_status = [UIImage imageNamed:@"bg_status_1"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:bg_status];
+    imageView.center = CGPointMake(SCREEN_WIDTH/2, bg_status.size.height/2+20);
+    [self.view addSubview:imageView];
 
+    CGFloat _height = bg_status.size.height+10;
+    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(40, _height+20, SCREEN_WIDTH/2-40, 20)];
+    label1.text = @"进水水质/TDS:90";
+    label1.textColor = [UIColor grayColor];
+    label1.font = FONT11;
+    label1.textAlignment = NSTextAlignmentLeft;
+    [self.view addSubview:label1];
+
+    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2+20, _height+20, SCREEN_WIDTH/2-20, 20)];
+    label2.text = @"设备编号:123";
+    label2.textColor = [UIColor grayColor];
+    label2.font = FONT11;
+    label2.textAlignment = NSTextAlignmentLeft;
+    [self.view addSubview:label2];
+
+    UILabel *label3 = [[UILabel alloc] initWithFrame:CGRectMake(40, _height+40, SCREEN_WIDTH/2-40, 20)];
+    label3.text = @"出水水质/TDS:12";
+    label3.textColor = [UIColor grayColor];
+    label3.font = FONT11;
+    label3.textAlignment = NSTextAlignmentLeft;
+    [self.view addSubview:label3];
+
+    UILabel *label4 = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2+20, _height+40, SCREEN_WIDTH/2-20, 20)];
+    label4.text = @"累计用水量:2L";
+    label4.textColor = [UIColor grayColor];
+    label4.font = FONT11;
+    label4.textAlignment = NSTextAlignmentLeft;
+    [self.view addSubview:label4];
 }
 
 -(void)deviceConfigAction:(id)sender {
