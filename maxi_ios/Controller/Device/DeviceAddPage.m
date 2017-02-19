08fd59834a17454a48a156a2627380ee7e8a174a
沈斌 <bluelazysb@hotmail.com>
2017-02-19 11:02:45 +0800

    updates

--- maxi_ios/Controller/Device/DeviceAddPage.m
+++ maxi_ios/Controller/Device/DeviceAddPage.m
@@ -5,6 +5,7 @@
 
 #import "DeviceAddPage.h"
 #import "DeviceMainPage.h"
+#import "TPKeyboardAvoidingScrollView.h"
 
 @interface DeviceAddPage() {
 
@@ -26,10 +27,12 @@
     UIBarButtonItem *leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     self.navigationItem.leftBarButtonItem = leftBarButtonItem;
 
+    TPKeyboardAvoidingScrollView *containerView = [[TPKeyboardAvoidingScrollView alloc] initWithFrame:self.view.frame];
+
     UIImage *bg_image = [UIImage imageNamed:@"icon_wifi"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:bg_image];
     imageView.center = CGPointMake(SCREEN_WIDTH/2, bg_image.size.height/2+30);
-    [self.view addSubview:imageView];
+    [containerView addSubview:imageView];
 
     CGFloat _height = bg_image.size.height+30;
     UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, _height+30, SCREEN_WIDTH, 30)];
@@ -37,32 +40,35 @@
     label1.textColor = [UIColor grayColor];
     label1.font = FONT18;
     label1.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:label1];
+    [containerView addSubview:label1];
 
     UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(0, _height+60, SCREEN_WIDTH, 30)];
     label2.text = @"连接一个可用的WIFI，让设备接入网络";
     label2.textColor = [UIColor grayColor];
     label2.font = FONT12;
     label2.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:label2];
+    [containerView addSubview:label2];
 
     UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(20, _height+90, SCREEN_WIDTH-40, 100)];
     imageView2.image = [UIImage imageNamed:@"bg_wifi"];
-    [self.view addSubview:imageView2];
+    [containerView addSubview:imageView2];
 
     wifiNameTextField = [[UITextField alloc] initWithFrame:CGRectMake(25, _height+90, SCREEN_WIDTH-50, 50)];
     wifiNameTextField.placeholder = @"WIFI名称";
-    [self.view addSubview:wifiNameTextField];
+    [containerView addSubview:wifiNameTextField];
 
     wifiPswTextField = [[UITextField alloc] initWithFrame:CGRectMake(25, _height+140, SCREEN_WIDTH-50, 50)];
     wifiPswTextField.placeholder = @"请输入密码";
-    [self.view addSubview:wifiPswTextField];
+    [containerView addSubview:wifiPswTextField];
 
     UIImageView *imageView3 = [[UIImageView alloc] initWithFrame:CGRectMake(20, SCREEN_HEIGHT-130, SCREEN_WIDTH-40, 40)];
     imageView3.image = [UIImage imageNamed:@"btn_next"];
     imageView3.userInteractionEnabled = YES;
     [imageView3 addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doAddDevice:)]];
-    [self.view addSubview:imageView3];
+    [containerView addSubview:imageView3];
+
+    containerView.contentSize = CGSizeMake(self.view.frame.size.width, SCREEN_HEIGHT);
+    [self.view addSubview:containerView];
 }
 
 -(void)doAddDevice:(UIGestureRecognizer *)gestureRecognizer {
--- maxi_ios/Views/MaxiTableViewCell.m
+++ maxi_ios/Views/MaxiTableViewCell.m
@@ -9,6 +9,8 @@
 @implementation MaxiTableViewCell
 
 //http://blog.sina.com.cn/s/blog_65cbfb2b0101cd60.html
+//http://www.cnblogs.com/wendingding/p/3761730.html
+//http://blog.csdn.net/u012350430/article/details/51181728
 
 - (void)layoutSubviews {
     [super layoutSubviews];
